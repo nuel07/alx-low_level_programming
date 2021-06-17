7dd5d18999efd7344b832126e69bb7a32057e1ca
nuel07 <yehanganep@gmail.com>
2021-06-17 08:26:57 -0700

    betty correcion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-/*a simple function to print out given text*/
+/**
+ * a simple function to print out given text
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
+  /**
+   * a return  statement to stop the program
+   */
   return (0);
 }
