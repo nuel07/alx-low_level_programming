0d663701f73d929271bc0f5c620dd1adff7d936a
nuel07 <yehanganep@gmail.com>
2021-06-17 13:16:29 -0700

    sizing up

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+/**
+ * main - entry point
+ * fwrite - print function
+ * Return: return 1
+ */  
+int main() 
+{ 
+  fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13); 
+  return (1); 
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 /**
  * main - a function to show data type sizes
+ * Return: stops the program
  */
 int main(void)
 {
-  sizeof(int);
-  sizeof(char);
-  sizeof(long);
-  sizeof(float);
-  /*
-   * return statement to stop the program
-   */        
+  int int_type;
+  char C;
+  float float_type;
+  long long_type;
+  double d;
+  sizeof(int_type);
+  sizeof(C);
+  sizeof(long_type);
+  sizeof(float_type);
+  sizeof(d); 
   return (0);
 }
