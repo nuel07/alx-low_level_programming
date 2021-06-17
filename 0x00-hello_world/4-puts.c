70d7e66db7fe1f79fd287bff8b54f3c00b25b8b7
nuel07 <yehanganep@gmail.com>
2021-06-17 12:31:04 -0700

    betty style fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
  * main - a simple function to print out given text
+ * Return: a return statement to stop the program
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  /*
-   * a return  statement to stop the program
-   */
-  return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
  * main - this function will print out the given text
+ * Return: a return statement to stop the program
  */
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,");
-  /*
-   * return statement to stop the program
-   */
   return (0);
 }
