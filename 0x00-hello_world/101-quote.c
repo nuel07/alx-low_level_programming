43ec98dc3f27d3a5b9024a8644598600449cae2a
nuel07 <yehanganep@gmail.com>
2021-06-18 15:37:55 -0700

    quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h> 
+#include <unistd.h>
 /**
  * main - entry point
  * write - print function
  * Return: return 1
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main() 
-{ 
-  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13); 
-  return (1); 
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+return (1);
 }
