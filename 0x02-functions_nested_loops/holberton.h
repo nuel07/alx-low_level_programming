83c8aec36be348e04db388b3d4358ac2b503602a
nuel07 <yehanganep@gmail.com>
2021-06-22 03:29:42 -0700

    header file commit

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,9 @@
+/**
+ * File: holberton.h
+ * Auth: Promise Yehangane
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
