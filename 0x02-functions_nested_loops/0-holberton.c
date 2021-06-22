fda5e162284ef7192512e5b9569d13b2ea09a81f
nuel07 <yehanganep@gmail.com>
2021-06-22 03:18:45 -0700

    first task

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,19 @@
-#include<stdio.h>
+#include "holberton.h"
 /**
- * main - entry point
+ * main - entry point, a program that prints Holberton
  * Return: return 0 (Success)
  */
 int main(void)
 {
-printf("Holberton\n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
