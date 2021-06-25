224da4c580f306369e87793e61b63cadcde6a91a
nuel07 <yehanganep@gmail.com>
2021-06-25 02:19:28 -0700

    debug fixes

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,5 +5,5 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-
-#endif /* HOLBERTON_H */
+void print_remaining_days(int month, int day, int year);
+#endif /* HOLBERTON_He */
