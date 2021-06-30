37909da24ae898fdade6cc8dedbc7602da3fbda6
nuel07 <yehanganep@gmail.com>
2021-06-30 08:15:46 -0700

    corrections

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _strcpy - Copies a string pointed to by @src, including the
  *           terminating null byte, to a buffer pointed to by @dest.
@@ -13,7 +12,7 @@ char *_strcpy(char *dest, const char *src)
 int index = 0;
 while (src[index])
 {
-dest[index] = src[index];
+*dest[index] = src[index];
 index++;
 }
 return (dest);
