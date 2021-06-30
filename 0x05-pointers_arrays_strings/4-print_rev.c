6d7d0c489ce13a5f9d38a1f1c851f6edefd0204a
nuel07 <yehanganep@gmail.com>
2021-06-30 08:09:42 -0700

    print_rev correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,10 +6,13 @@
  */
 void print_rev(char *s)
 {
-int len = 0, index;
-while (s[index++])
-len++;
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
-_putchar('\n');
+  int len = 0, index;
+
+  while (s[index++])
+    len++;
+
+  for (index = len - 1; index >= 0; index--)
+    _putchar(s[index]);
+
+  _putchar('\n');
 }
