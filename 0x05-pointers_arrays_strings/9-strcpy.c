6023ba0d6e3d278e2c58c1fe03561fdb53b9de73
nuel07 <yehanganep@gmail.com>
2021-06-30 08:18:35 -0700

    strcpy.c corrections

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,12 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 int index = 0;
 while (src[index])
 {
-*dest[index] = src[index];
+dest[index] = src[index];
 index++;
 }
 return (dest);
