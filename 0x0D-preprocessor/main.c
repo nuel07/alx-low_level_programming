a57d11c6ed1735e1c73f1badb0d68926e426c0bb
nuel07 <yehanganep@gmail.com>
2021-07-19 23:34:49 -0700

    a few fixes

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -11,9 +11,11 @@
  */
 int main(void)
 {
-  int s;
+  int i;
+  int j;
 
-  s = SUM(98, 1024);
-  printf("%d\n", s);
+  i = ABS(-98) * 10;
+  j = ABS(98) * 10;
+  printf("%d, %d\n", i, j);
   return (0);
 }
