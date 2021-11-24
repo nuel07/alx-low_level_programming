33a8737723d1250997cea319aac6ec413f297bdd
nuel07 <yehanganep@gmail.com>
2021-11-24 11:51:56 -0800

    technical interview task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 3
+"""An Island's perimeter"""
+
+def island_perimeter(grid):
+    """calculates and returns perimeter of an island described in grid
+    Args(list): list of integers that define the island
+    Return: int
+    """
+    side = 0
+    edge = 0
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[i][j] == 1:
+                side += 1
+                if x != 0 and grid[x-1][y] == 1:
+                    edge += 1
+                if y != 0 and grid[x][y-1] == 1:
+                    edge += 1
+
+    return 4*side - 2*edge
