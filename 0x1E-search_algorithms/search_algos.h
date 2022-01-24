4d10801d439c08f0129a47b92e7bd1faadb403a7
nuel07 <yehanganep@gmail.com>
2022-01-24 01:48:00 -0800

    header file data struct documentation

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,17 @@
 
 /* helpers */
 void print_array(int *array, size_t inc, size_t size);
-
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
 typedef struct skiplist_s
 {
 int n;
@@ -14,7 +24,16 @@ size_t index;
 struct skiplist_s *next;
 struct skiplist_s *express;
 } skiplist_t;
-
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 int n;
